82e79f93826bd968953ca503b4ab713974b8d4b8
Thomas Roess <thomas_roess@hotmail.fr>
2020-03-17 17:14:13 +0100

    update

--- Algo_PHP/Partie_2/Exo13.php
+++ Algo_PHP/Partie_2/Exo13.php
@@ -17,9 +17,13 @@
             self::$_id++;
             $this->_selfid = self::$_id ;
         }
+        public function short()
+        {
+            return $this->short();
+        }
         public function demarrer()
         {
-            echo "Le véhicule ".$this->_marque." ".$this->_model." démare<br>";
+            echo "Le véhicule ".$this->short()." démare<br>";
             $this->_start = true;
         }
         public function accelerer($vitesse)
@@ -27,22 +31,22 @@
             if($this->_start)
             {
                 $this->_vitesseActuelle = $this->_vitesseActuelle + $vitesse;
-                echo "Le véhicule ".$this->_marque." ".$this->_model." accélère de ".$vitesse." km/h et roule à ".$this->_vitesseActuelle."<br>";
+                echo "Le véhicule ".$this->short()." accélère de ".$vitesse." km/h et roule à ".$this->_vitesseActuelle."<br>";
             }
             else   
-                echo "Le véhicule ".$this->_marque." ".$this->_model." veut accélèrer de ".$vitesse." mais il n'est pas démaré<br>";
+                echo "Le véhicule ".$this->short()." veut accélèrer de ".$vitesse." mais il n'est pas démaré<br>";
         }
         public function ralentir($vitesse)
         {
             if($this->_start)
             {
                 $this->_vitesseActuelle = $this->_vitesseActuelle - $vitesse;
-                echo "Le véhicule ".$this->_marque." ".$this->_model." ralenti de ".$vitesse." km/h et roule à ";
+                echo "Le véhicule ".$this->short()." ralenti de ".$vitesse." km/h et roule à ";
                 if($this->_vitesseActuelle <=0)
                 {
                     $this->_vitesseActuelle = 0;
                     echo $this->_vitesseActuelle."<br>";
-                    echo "Le véhicule ".$this->_marque." ".$this->_model." à atteind 0 Km/h et s'arrète <br>";
+                    echo "Le véhicule ".$this->short()." à atteind 0 Km/h et s'arrète <br>";
                     $this->_start = false;
                 }
                 else   
@@ -50,24 +54,24 @@
                 
             }
             else   
-                echo "Le véhicule ".$this->_marque." ".$this->_model." veut ralentir de ".$vitesse." mais il n'est pas démaré<br>";
+                echo "Le véhicule ".$this->short()." veut ralentir de ".$vitesse." mais il n'est pas démaré<br>";
         }
         public function stopper()
         {
-            echo "Le véhicule ".$this->_marque." ".$this->_model." est stoppé<br>";
+            echo "Le véhicule ".$this->short()." est stoppé<br>";
             $this->_start = false;
             $this->_vitesseActuelle = 0;
         }
         public function vitesse()
         {
-            echo "Le véhicule ".$this->_marque." ".$this->_model." roule à ".$this->_vitesseActuelle."<br>";
+            echo "Le véhicule ".$this->short()." roule à ".$this->_vitesseActuelle."<br>";
 
         }
-        public function statut()
+        public function __toString()
         {
             echo "<br><br>Infos véhicule ".$this->_selfid."
             <br>***********
-            <br>Nom et modèle du véhicule : ".$this->_marque." ".$this->_model."
+            <br>Nom et modèle du véhicule : ".$this->short()."
             <br>Nombres de portes : ".$this->_nbPortes."
             <br>Le véhicule est ";
             if($this->_start)
@@ -80,9 +84,9 @@
     }
 
 
-    $v1 = new Voiture("Peugeot","408","5");
-    $v2 = new Voiture("Citroën","C4","3");
-    $v3 = new Voiture("Volkswagen","Passat","5");
+    $v1 = new Voiture("Peugeot","408",5);
+    $v2 = new Voiture("Citroën","C4",3);
+    $v3 = new Voiture("Volkswagen","Passat",5);
     $v1->demarrer();
     $v1->accelerer(50);
     $v1->accelerer(20);
@@ -98,14 +102,7 @@
     $v3->ralentir(20);
     $v3->demarrer();
     $v3->accelerer(200);
-    $v1->statut();
-    $v2->statut();
-    $v3->statut();
-    /*Créer une classe Voiture possédantles propriétés suivantes:
-    marque, modèle, nbPorteset vitesseActuelle
-    ainsi que les méthodes demarrer( ), accelerer( )et stopper( )
-    en plus  des  accesseurs  (get)  et  mutateurs  (set) traditionnels.
-    La  vitesse  initiale  de  chaque  véhicule instancié est de 0.
-    Une méthode personnalisée pourra afficher toutes les informations d’un véhicule. 
-    v1 "Peugeot","408",5v2 "Citroën","C4",3*/
+    echo $v1;
+    echo $v2;
+    echo $v3;
 ?>
