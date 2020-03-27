ad7a965c84fd035b60091241f002a3fdae922aaf
Thomas Roess <thomas_roess@hotmail.fr>
2020-03-27 16:54:26 +0100

    Update Exo banque

--- Algo_PHP/Extra/Exobanque/class_Titulaire.php
+++ Algo_PHP/Extra/Exobanque/class_Titulaire.php
@@ -41,41 +41,5 @@
                 $this->_compte[] = $compte1;
             }
         }
-    class Compte{
-        private $_lib;
-        private $_solde;
-        private $_devise;
-        private $_titulaire;
-
-        public function __construct($lib,$solde,$devise,$titulaire)
-        {
-            $this->_lib = $lib;
-            $this->_solde = $solde;
-            $this->_devise = $devise;
-            $this->_titulaire = $titulaire;
-            
-            $titulaire->setCompte($this);
-
-        }
-        public function getLib()
-        {
-            return $this->_lib;
-        }
-        public function getSolde()
-        {
-            return $this->_solde;
-        }
-        public function getDevise()
-        {
-            return $this->_devise;
-        }
-        public function getInfo()
-        {
-            $retour ="<br>Le compte".$this->_lib." appartient à ".$this->_titulaire->getNom();
-            return $retour;
-        }
-        
-    }
-
     
 ?>
--- Algo_PHP/Extra/Exobanque/exo.php
+++ Algo_PHP/Extra/Exobanque/exo.php
@@ -1,12 +1,17 @@
 <?php
-      include "class_banque.php";
+      include "class_Titulaire.php";
+      include "class_Compte.php";
+
 
       $jean = new Titulaire("Valjean","Jean","1988-11-19","Munster");
       $compte1 = new Compte("test",50,"euro",$jean);
       $compte2 = new Compte("test2",800,"franc",$jean);
-
+      $michel = new Titulaire("MichMuch","Michel","1988-11-19","Munster");
+      $compte3 = new Compte("test3",1,"rouble",$michel);
       echo $jean->getCompte();
       echo $compte1->getInfo();
+      echo $michel->getCompte();
+      echo $compte3->getInfo();
 
 
  ?>
