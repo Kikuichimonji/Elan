a43961fd3a182322bf2977a6d72a213bf73035a0
Thomas Roess <thomas_roess@hotmail.fr>
2020-03-27 11:49:06 +0100

    asdf

--- Algo_PHP/Partie_2/Exo7.php
+++ Algo_PHP/Partie_2/Exo7.php
@@ -1,15 +1,13 @@
 <?php
-    $elements = array("1"=>"Choix 1","2"=>"Choix 2","3"=>"Choix 3");
     $check = array("Choix 1"=>"","Choix 2"=>"checked ","Choix 3"=>"");
-    function genererCheckbox($tab,$check){     // A function that generate checkbox out of an array
+    function genererCheckbox($check){     // A function that generate checkbox out of an array
         $retour = "<form>";
-        foreach($tab as $key => $val)
+        foreach($check as $key => $val)
         {
-           $retour=$retour."<input type='checkbox' id='".$key."' value='".$key."' ".$check[$val].">";
-           $retour=$retour."<label>".$val."</label><br>";
+           $retour.="<input type='checkbox' ".$val."><label>".$key."</label><br>";
         }
-        $retour=$retour."</form>";
+        $retour.="</form>";
         return $retour;
     }
-    echo genererCheckbox($elements,$check);;
+    echo genererCheckbox($check);
 ?>
