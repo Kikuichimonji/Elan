42738d2a4a5dc3acd544465dc4099e5ec6ab336e
Thomas Roess <thomas_roess@hotmail.fr>
2020-04-01 15:09:33 +0200

    Update pendu

---
+++ Pendu/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="pendu.css" />
+    <title>Document</title>
+</head>
+<body> 
+    <?php 
+        $liste=["test","bonjour","virgile","llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch"];
+        $mot = $liste[array_rand($liste,1)];
+    ?>
+    <div id="enchiladas">
+            <h1>Le jeu du PENDU</h1>
+        <div id="mainjeu">
+            <div id="dessin">
+                dessin
+            </div>
+            <div id="mot"> 
+                <?php 
+                    for($i=0;$i<strlen($mot);$i++)
+                        echo "<span class='lettre' id='".$i."'>_</span>";
+                ?>
+            </div>
+            <div id="texte">
+                
+            </div>
+            <div id="reponse"><?php echo $mot ?></div>
+        </div>
+    </div>
+    
+    <script src="pendu.js"></script>
+</body>
+</html>
+    
---
+++ Pendu/pendu.js
@@ -0,0 +1,48 @@
+var albhabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var coup = 8;
+var mot = document.getElementById("reponse").innerText;
+var restant = mot.length;
+var dessin = document.getElementById("dessin");
+var clavier = document.getElementsByClassName("clavier");
+albhabet.forEach(element => {
+    document.getElementById("texte").innerHTML += "<span class='clavier'>"+element+"</span>";
+});
+
+
+for(i=0;i<clavier.length;i++){
+    clavier[i].addEventListener("click",clickClavier);
+}
+function clickClavier()
+{
+    var bool = false;
+    for(j=0;j<mot.length;j++)
+    {
+        if(mot[j] == this.innerHTML)
+        {
+            document.getElementById(j).innerHTML= this.innerHTML;
+            bool = true;
+            restant--;
+        }
+        if(bool)
+            this.className = "clavier true";
+        else 
+            this.className = "clavier false";
+    }
+    if(!bool)
+        coup--;
+    dessin.innerHTML = coup;
+    if(coup <= 0)
+    {
+        dessin.innerHTML = "PERDU";
+        [].forEach.call(clavier,function(perdu){
+            perdu.removeEventListener("click",clickClavier)
+        });
+    }
+    else if(restant == 0)
+    {
+        dessin.innerHTML = "GAGNER";
+        [].forEach.call(clavier,function(perdu){
+            perdu.removeEventListener("click",clickClavier)
+        });
+    }
+}
