83b8be79c8537e2415d5379eb6b87636f0cba585
Thomas Roess <thomas_roess@hotmail.fr>
2020-04-01 08:59:50 +0200

    update 31/03

--- ExoJS/Echiquier/chess.js
+++ ExoJS/Echiquier/chess.js
@@ -1,16 +1,16 @@
 var mainGrille = document.getElementById("grille");
 var grilleCases = document.getElementsByClassName("case");
 var tailleGrille = 8;
-var couleur = "white";
+var couleur = "grey";
 var couleurPions ="white";
 var tabPieces =["tour","cavalier","fou","king","queen","fou","cavalier","tour","pion"];
 
 for(i=0;i<tailleGrille;i++)
 {
-    couleur = couleur == "white" ? "black" : "white";
+    couleur = couleur == "grey" ? "brown" : "grey";
     for(j=0;j<tailleGrille;j++)
     {
-        couleur = couleur == "white" ? "black" : "white";
+        couleur = couleur == "grey" ? "brown" : "grey";
         if(i==1 || i==6)
             mainGrille.innerHTML += "<div class='case' id="+i+j+" style='background-color :"+couleur+"'><img src='"+couleurPions+" "+tabPieces[8]+".png'></div>";
         else if(i==0 || i==7)
@@ -20,15 +20,5 @@ for(i=0;i<tailleGrille;i++)
     }
     couleurPions = i > 4 ? "black" : "white";
 
-}
-
-function setViewport(img, x, y, width, height) {
-    img.style.left = "-" + x + "px";
-    img.style.top  = "-" + y + "px";
-
-    if (width !== undefined) {
-        img.parentNode.style.width  = width  + "px";
-        img.parentNode.style.height = height + "px";
-    }
 }
 
